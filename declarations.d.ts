b961c2a240f0f18639c47838129a769f72f1136a
unknown <wisnuyeahfaizal@gmail.com>
2023-10-30 19:48:19 +0700

    fix: svg tsc error

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,8 +1,3 @@
-declare module '*.svg' {
-  const content: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default content;
-}
-
 declare module '*.png' {
   const content: string;
   export default content;
