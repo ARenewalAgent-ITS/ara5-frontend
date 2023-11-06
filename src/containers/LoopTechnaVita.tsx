6883a0e4c15c122976f8a48e7c5b48e09339d41c
Michael Wayne <michaelwaynewibisono@gmail.com>
2023-11-06 21:52:49 +0700

    feat: deploy techna vita loop

--- src/containers/LoopTechnaVita.tsx
+++ src/containers/LoopTechnaVita.tsx
@@ -3,21 +3,22 @@ import React from 'react';
 import Image from 'next/image';
 import Marquee from 'react-fast-marquee';
 import LogoAra from '../components/layouts/nav-img/Group 934.svg';
+import Typography from '@/components/Typography';
 
 function LoopTechnaVita() {
   return (
     <>
       <div className='bg-[#393737] w-[100%]'>
         <Marquee loop={0} speed={50} delay={0} direction='right' className=''>
-          <div className='flex'>
+          <div className='flex py-2'>
             {Array(7)
               .fill(null)
               .map((_, index) => (
                 <div key={index} className='flex'>
                   <Image src={LogoAra} alt='logo' className='w-[1rem]'></Image>
-                  <div className='mr-4 ml-2 flex justify-center items-center text-[#ffffff] text-xl font-semibold italic'>
+                  <Typography className='mr-4 ml-2 flex justify-center items-center text-[#ffffff] text-xl font-semibold italic'>
                     ARA 5.0 - Techna Vita
-                  </div>
+                  </Typography>
                 </div>
               ))}
           </div>
