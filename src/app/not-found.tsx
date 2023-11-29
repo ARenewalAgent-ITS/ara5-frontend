0fde469c869b4f30e410c31bd9cbe7dcef832a67
fioreenza <fiorenzanalle05@gmail.com>
2023-11-30 05:16:12 +0700

    fix: formatting issue 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 import Aos from 'aos';
 import React, { useEffect } from 'react';
 import { MdHome } from 'react-icons/md';
@@ -11,11 +11,10 @@ import Typography from '@/components/Typography';
 import clsxm from '@/lib/clsxm';
 
 export default function NotFound() {
-
   useEffect(() => {
-  Aos.init();
+    Aos.init();
   }, []);
-  
+
   return (
     <>
       <div>
@@ -29,7 +28,8 @@ export default function NotFound() {
               alt='background'
               width={238.7}
               height={108.9}
-              data-aos='fade-right' data-aos-delay='400'
+              data-aos='fade-right'
+              data-aos-delay='400'
               className='absolute lg:w-[280px] lg:-left-4 lg:top-48 md:w-[230px] sm:w-[200px] top-24 w-[165px]'
             />
             <NextImage
@@ -37,7 +37,8 @@ export default function NotFound() {
               alt='background'
               width={109.54}
               height={37.55}
-              data-aos='fade-right' data-aos-delay='400'
+              data-aos='fade-right'
+              data-aos-delay='400'
               className='absolute lg:left-36 lg:top-[23rem] md:w-[160px] sm:inline-block sm:w-[140px] top-[21rem] -left-5 w-[100px]'
             />
             <NextImage
@@ -45,7 +46,8 @@ export default function NotFound() {
               alt='background'
               width={42.25}
               height={75}
-              data-aos='fade-right' data-aos-delay='400'
+              data-aos='fade-right'
+              data-aos-delay='400'
               className='absolute hidden lg:inline-block top-[32rem] left-24'
             />
             <NextImage
@@ -53,7 +55,8 @@ export default function NotFound() {
               alt='background'
               width={266.93}
               height={83.97}
-              data-aos='fade-left' data-aos-delay='400'
+              data-aos='fade-left'
+              data-aos-delay='400'
               className='absolute lg:w-[320px] lg:top-56 lg:-right-32 md:w-[225px] sm:w-[195px] top-36 -right-2 w-[160px]'
             />
             <NextImage
@@ -61,7 +64,8 @@ export default function NotFound() {
               alt='background'
               width={124.5}
               height={57.47}
-              data-aos='fade-left' data-aos-delay='400'
+              data-aos='fade-left'
+              data-aos-delay='400'
               className='absolute lg:right-32 lg:top-[24rem] sm:w-[135px] sm:top-[27rem] top-[26rem] -right-6 w-[100px]'
             />
             <NextImage
@@ -69,7 +73,8 @@ export default function NotFound() {
               alt='background'
               width={36.25}
               height={64.75}
-              data-aos='fade-left' data-aos-delay='400'
+              data-aos='fade-left'
+              data-aos-delay='400'
               className='absolute hidden lg:inline-block top-[30rem] right-14'
             />
             <NextImage
@@ -77,7 +82,8 @@ export default function NotFound() {
               alt='background'
               width={27}
               height={23}
-              data-aos='fade-right' data-aos-delay='400'
+              data-aos='fade-right'
+              data-aos-delay='400'
               className='absolute lg:hidden inline-block md:left-24 md:top-[28rem] sm:left-20 top-[30rem] left-8 w-[30px]'
             />
             <NextImage
@@ -85,7 +91,8 @@ export default function NotFound() {
               alt='background'
               width={26}
               height={28}
-              data-aos='fade-right' data-aos-delay='400'
+              data-aos='fade-right'
+              data-aos-delay='400'
               className='absolute lg:hidden inline-block md:left-36 md:top-[34rem] sm:left-32 top-[36rem] left-20 w-[30px]'
             />
             <NextImage
@@ -93,7 +100,8 @@ export default function NotFound() {
               alt='background'
               width={24}
               height={21}
-              data-aos='fade-left' data-aos-delay='400'
+              data-aos='fade-left'
+              data-aos-delay='400'
               className='absolute lg:hidden inline-block md:right-28 sm:right-20 top-[34rem] right-16 w-[30px]'
             />
           </div>
@@ -104,11 +112,16 @@ export default function NotFound() {
                 alt='kayu'
                 width={355}
                 height={97.87}
-                data-aos='fade-down' data-aos-delay='400'
+                data-aos='fade-down'
+                data-aos-delay='400'
                 className='lg:w-[490px] md:w-[380px] sm:w-[350px] w-[280px]'
               />
             </div>
-            <div className='flex flex-col items-center text-center z-20' data-aos='zoom-in' data-aos-delay='400'>
+            <div
+              className='flex flex-col items-center text-center z-20'
+              data-aos='zoom-in'
+              data-aos-delay='400'
+            >
               <Typography
                 weight='bold'
                 className={clsxm(
@@ -171,7 +184,8 @@ export default function NotFound() {
                 alt='background'
                 width={306.35}
                 height={98.83}
-                data-aos='fade-right' data-aos-delay='400'
+                data-aos='fade-right'
+                data-aos-delay='400'
                 className='absolute lg:w-[360px] lg:-bottom-40 lg:-left-12 md:w-[300px] md:-bottom-20 w-[230px] -bottom-16 -left-5'
               />
               <NextImage
@@ -179,7 +193,8 @@ export default function NotFound() {
                 alt='background'
                 width={50}
                 height={74}
-                data-aos='fade-up' data-aos-delay='400'
+                data-aos='fade-up'
+                data-aos-delay='400'
                 className='absolute lg:-bottom-48 lg:w-[120px] lg:right-4 md:w-[65px] md:-bottom-32 md:right-36 -bottom-28 right-28 w-[50px] max-[400px]:hidden'
               />
               <NextImage
@@ -187,7 +202,8 @@ export default function NotFound() {
                 alt='background'
                 width={284.85}
                 height={492}
-                data-aos='fade-left' data-aos-delay='400'
+                data-aos='fade-left'
+                data-aos-delay='400'
                 className='absolute xl:-bottom-44 lg:w-[250px] md:w-[160px] md:-bottom-44 w-[120px] -bottom-40 -right-3'
               />
               <NextImage
@@ -195,7 +211,8 @@ export default function NotFound() {
                 alt='background'
                 width={117}
                 height={64}
-                data-aos='fade-up' data-aos-delay='400'
+                data-aos='fade-up'
+                data-aos-delay='400'
                 className='absolute lg:w-[120px] lg:right-80 lg:-bottom-40 md:w-[80px] md:-bottom-16 md:right-40 w-[70px] -bottom-14 right-32 max-[400px]:hidden'
               />
               <NextImage
@@ -203,7 +220,8 @@ export default function NotFound() {
                 alt='background'
                 width={47.5}
                 height={15}
-                data-aos='fade-up' data-aos-delay='400'
+                data-aos='fade-up'
+                data-aos-delay='400'
                 className='absolute lg:hidden inline_block md:w-[65px] md:left-48 md:-bottom-28 w-[50px] -bottom-20 left-36'
               />
               <NextImage
@@ -211,7 +229,8 @@ export default function NotFound() {
                 alt='background'
                 width={29}
                 height={36}
-                data-aos='fade-right' data-aos-delay='400'
+                data-aos='fade-right'
+                data-aos-delay='400'
                 className='absolute lg:hidden inline_block md:w-[45px] md:-bottom-36 w-[35px] -bottom-36 left-0'
               />
             </div>
