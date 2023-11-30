56e52c6220897e48a4b49e317047ac5d3277a41b
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2023-11-30 19:33:14 +0700

    feat: olimpit & ctf faq section

---
+++ src/containers/ctfPage/CtfFaqSection.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import FAQCard from '@/components/FAQCard';
+import NextImage from '@/components/NextImage';
+
+interface TCTFFaqData {
+  svg: string;
+  q: string;
+  a: string;
+}
+
+const faqs: TCTFFaqData[] = [
+  {
+    svg: '/svg/ctfpage/faq-icons/1.svg',
+    q: 'CTF ARA 5.0 menggunakan style apa?',
+    a: 'CTF ARA 5.0 menggunakan style Jeopardy untuk penyisihan.',
+  },
+  {
+    svg: '/svg/ctfpage/faq-icons/5.svg',
+    q: 'Apakah ada ada batasan submit flag?',
+    a: 'Tidak ada, namun peserta dilarang untuk melakukan bruteforcing.',
+  },
+  {
+    svg: '/svg/ctfpage/faq-icons/2.svg',
+    q: 'Apakah ada babak final dalam CTF ARA 5.0? dan menggunakan style apa?',
+    a: 'Ada, babak final akan diadakan secara offline di ITS dengan menggunakan format?',
+  },
+  {
+    svg: '/svg/ctfpage/faq-icons/3.svg',
+    q: 'Apakah diperbolehkan untuk melakukan brute force?',
+    a: 'Brute force terhadap server tidak diperbolehkan. Apabila ditemukan adanya tindakan bruteforcing, maka panitia berhak mendiskualifikasi peserta beserta tim nya.',
+  },
+  {
+    svg: '/svg/ctfpage/faq-icons/4.svg',
+    q: 'Bagaimana jika ada kendala teknis dalam pembuatan writeup? apakah ada toleransi keterlambatan?',
+    a: 'Tidak ada toleransi keterlambatan dalam pengumpulan writeup dengan alasan apapun.',
+  },
+
+  {
+    svg: '/svg/ctfpage/faq-icons/5.svg',
+    q: 'Apakah diperbolehkan menggunakan automation-tools seperti SQLMAP, dirbuster, dan lainnya?',
+    a: 'Penggunaan automations-tools terhadap server, seperti bruteforcing direktori menggunakan dirbuster, tidak diperbolehkan. Namun, penggunaan automations-tools selain terhadap server, seperti cracking password menggunakan john, diperbolehkan.',
+  },
+];
+
+export default function CtfFaqSection() {
+  return (
+    <section className='ctf-faq-section mt-36 bg-[#09182C]'>
+      <div className='upper-faq flex justify-center items-center mb-20'>
+        <NextImage
+          src={'/svg/ctfpage/faq.svg'}
+          width={418}
+          height={53}
+          alt='faq panel'
+        />
+      </div>
+      <div className='faq-cards grid grid-cols-1 lg:grid-cols-4 gap-3 md:gap-7 px-8'>
+        {faqs.map((faq, id) => (
+          <FAQCard
+            key={id}
+            q={faq.q}
+            a={faq.a}
+            svg={faq.svg}
+            cardNumber={id}
+            length={faqs.length}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ src/containers/olimpPage/OlimpFaqSection.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import FAQCard from '@/components/FAQCard';
+import NextImage from '@/components/NextImage';
+
+interface TOlimpFaqData {
+  svg: string;
+  q: string;
+  a: string;
+}
+
+const faqs: TOlimpFaqData[] = [
+  {
+    svg: '/svg/olimppage/faq-icons/1.svg',
+    q: 'Siapa saja yang boleh mengikuti Olimpiade ARA 5.0?',
+    a: 'Siswa-siswi  SMA/K sederajat dibuktikan dengan Kartu Pelajar dan identitas yang sah.',
+  },
+  {
+    svg: '/svg/olimppage/faq-icons/2.svg',
+    q: 'Apa saja materi yang akan diujikan pada Olimpiade ARA 5.0?',
+    a: 'Materi yang diujikan yaitu Internet of Things dan Cyber Security. Untuk detail lebih lanjut terkait cakupan materi ataupun sub-bab yang diujikan akan diinformasikan lebih lanjut pada guidebook.',
+  },
+  {
+    svg: '/svg/olimppage/faq-icons/3.svg',
+    q: 'Seperti apa teknis pelaksanaan penyisihan dan final Olimpiade ARA 5.0?',
+    a: 'Pelaksanaan penyisihan dilakukan secara online lewat platform yang disediakan. Peserta harus join Zoom yang disediakan panitia saat mengerjakan penyisihan dengan teknis yang tertera di guidebook. Pelaksanaan Final Olimpiade ARA 5.0 dilaksanakan di Kampus ITS Sukolilo Surabaya.',
+  },
+  {
+    svg: '/svg/olimppage/faq-icons/4.svg',
+    q: 'Berapa banyak anggota yang harus dimiliki satu tim?',
+    a: 'Satu tim harus memiliki 2 orang anggota yang berasal dari satu sekolah yang sama.',
+  },
+  {
+    svg: '/svg/olimppage/faq-icons/5.svg',
+    q: 'Berapa banyak soal yang akan diberikan pada saat penyisihan dan final?',
+    a: 'Pada saat penyisihan akan diberikan soal sebanyak 100 butir dengan berbentuk pilihan ganda. Sedangkan pada saat final akan diberikan 10 soal berbentuk essay. Siswa-siswi  SMA/K sederajat dibuktikan dengan Kartu Pelajar dan identitas yang sah.',
+  },
+];
+
+export default function OlimFaqSection() {
+  return (
+    <section className='olimp-faq-section mt-36 bg-[#09182C]'>
+      <div className='upper-faq flex justify-center items-center mb-20'>
+        <NextImage
+          src={'/svg/olimppage/faq.svg'}
+          width={418}
+          height={53}
+          alt='faq panel'
+        />
+      </div>
+      <div className='faq-cards grid grid-cols-1 lg:grid-cols-4 gap-3 md:gap-7 px-8'>
+        {faqs.map((faq, id) => (
+          <FAQCard
+            key={id}
+            q={faq.q}
+            a={faq.a}
+            svg={faq.svg}
+            cardNumber={id}
+            length={faqs.length}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
