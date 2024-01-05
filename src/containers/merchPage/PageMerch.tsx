eb635a478100f5257ba3f3e68e7a758dbc406bf6
fioreenza <fiorenzanalle05@gmail.com>
2024-01-06 05:03:13 +0700

    fix: modify layout merch page

--- src/containers/merchPage/PageMerch.tsx
+++ src/containers/merchPage/PageMerch.tsx
@@ -1,9 +1,19 @@
 'use client';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/swiper-bundle.min.css';
 
 import * as React from 'react';
 import { useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
+import { PiShoppingCartSimpleFill } from 'react-icons/pi';
 
+import SwiperCore, { A11y, Autoplay, Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import Navbar from '@/components/layouts/Navbar';
 import NextImage from '@/components/NextImage';
 import Typography from '@/components/Typography';
 
@@ -12,6 +22,7 @@ interface ProductData {
   name: string;
   price: string;
   imageSrc: string;
+  category: string;
 }
 
 const products: ProductData[] = [
@@ -19,132 +30,212 @@ const products: ProductData[] = [
     id: 1,
     name: 'Lanyard Arlo',
     price: 'Rp10.000',
-    imageSrc: '/img/merchpage/frame.svg',
+    imageSrc: '/img/merchpage/frames.svg',
+    category: 'Lanyard',
   },
   {
     id: 2,
     name: 'Lanyard Arlo',
     price: 'Rp10.000',
-    imageSrc: '/img/merchpage/frame.svg',
+    imageSrc: '/img/merchpage/frames.svg',
+    category: 'Lanyard',
   },
   {
     id: 3,
     name: 'Lanyard Arlo',
     price: 'Rp10.000',
-    imageSrc: '/img/merchpage/frame.svg',
+    imageSrc: '/img/merchpage/frames.svg',
+    category: 'Lanyard',
   },
   {
     id: 4,
     name: 'Lanyard Arlo',
     price: 'Rp10.000',
-    imageSrc: '/img/merchpage/frame.svg',
+    imageSrc: '/img/merchpage/frames.svg',
+    category: 'Lanyard',
   },
   {
     id: 5,
     name: 'Lanyard Arlo',
     price: 'Rp10.000',
-    imageSrc: '/img/merchpage/frame.svg',
+    imageSrc: '/img/merchpage/frames.svg',
+    category: 'Lanyard',
   },
   {
     id: 6,
     name: 'Lanyard Arlo',
     price: 'Rp10.000',
-    imageSrc: '/img/merchpage/frame.svg',
+    imageSrc: '/img/merchpage/frames.svg',
+    category: 'Lanyard',
   },
   {
     id: 7,
     name: 'Lanyard Arlo',
     price: 'Rp10.000',
-    imageSrc: '/img/merchpage/frame.svg',
+    imageSrc: '/img/merchpage/frames.svg',
+    category: 'Lanyard',
   },
   {
     id: 8,
     name: 'Lanyard Arlo',
     price: 'Rp10.000',
-    imageSrc: '/img/merchpage/frame.svg',
+    imageSrc: '/img/merchpage/frames.svg',
+    category: 'Lanyard',
   },
 ];
 
-export default function PageMerch() {
-  const [showDropdown, setShowDropdown] = useState(false);
+const paginationStyle = {
+  '--swiper-pagination-color': '#212121',
+  '--swiper-pagination-bullet-inactive-color': '#212121',
+  '--swiper-pagination-bullet-inactive-opacity': '0.5',
+  '--swiper-pagination-bullet-active-opacity': '1',
+  '--swiper-pagination-bullet-size': '0.65rem',
+} as React.CSSProperties;
 
-  const handleDropdownClick = () => {
-    setShowDropdown(!showDropdown);
+const images = [
+  '/img/merchpage/banner.svg',
+  '/img/merchpage/banner2.svg',
+  '/img/merchpage/banner3.svg',
+  '/img/merchpage/banner4.svg',
+];
+
+export default function PageMerch() {
+  const [showCatDropdown, setShowCatDropdown] = useState(false);
+  const [showSortDropdown, setShowSortDropdown] = useState(false);
+
+  const CatDropdownClick = () => {
+    setShowCatDropdown(!showCatDropdown);
   };
+
+  const SortDropdownClick = () => {
+    setShowSortDropdown(!showSortDropdown);
+  };
+
+  SwiperCore.use([Autoplay]);
 
   return (
     <>
-      <section className='flex justify-center bg-transparent'>
-        <div className='w-full h-full flex flex-col'>
-          <div className='mx-10 sm:mx-16'>
-            <div className='flex justify-end items-end'>
+      <Navbar />
+      <Swiper
+        modules={[A11y, Navigation, Pagination]}
+        spaceBetween={0}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+        loop={true}
+        centeredSlides={true}
+        centeredSlidesBounds={true}
+        style={paginationStyle}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className='pt-20 flex justify-center items-center relative'>
               <NextImage
-                src={'/img/merchpage/shoppingcart.svg'}
-                alt='shoppingcart'
-                width={20}
-                height={20}
-                className='my-8 justify-end w-[25px] sm:w-[30px]'
+                src={image}
+                alt='frame'
+                width={616}
+                height={171}
+                className='cursor-pointer w-full'
               />
             </div>
-            <div className='flex justify-between'>
-              <div className='flex flex-col'>
-                <Typography
-                  weight='bold'
-                  font='poppins'
-                  className='text-success-1100 text-[28px] sm:text-[32px] md:text-[36px]'
-                >
-                  ARA 5.0
-                </Typography>
-                <Typography
-                  weight='bold'
-                  font='poppins'
-                  className='text-primary-600 text-[28px] sm:hidden'
-                >
-                  Lanyard
-                </Typography>
-                <Typography
-                  weight='bold'
-                  font='poppins'
-                  className='text-primary-600 text-[28px] sm:hidden'
-                >
-                  Merch
-                </Typography>
-                <Typography
-                  weight='bold'
-                  font='poppins'
-                  className='text-primary-600 hidden sm:block sm:text-[32px] md:text-[36px]'
-                >
-                  Lanyard Merch
-                </Typography>
-              </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <section className='flex justify-center bg-transparent mt-8'>
+        <div className='w-full h-full flex flex-col'>
+          <div className='mx-6 md:mx-8 lg:mx-12'>
+            <div className='flex justify-between items-center'>
+              <Typography
+                weight='bold'
+                font='poppins'
+                className='text-success-1100 text-[18px] sm:text-[24px] md:text-[28px]'
+              >
+                Katalog Merchandise
+              </Typography>
+              <NextImage
+                src='/img/merchpage/sorticon.svg'
+                alt='frame'
+                width={22}
+                height={12}
+                className='cursor-pointer block lg:hidden'
+              />
               <div
-                onClick={handleDropdownClick}
+                onClick={CatDropdownClick}
                 style={{ cursor: 'pointer' }}
-                className='sm:mt-16 mt-20 items-center flex-col box-content md:w-48 sm:w-36 w-28 h-4 p-2 border-[1px] border-whites-1100 rounded-md'
+                className='hidden z-10 cursor-pointer lg:block items-center flex-col bg-whites-100 lg:w-80 w-28 h-10 p-2 border-[1px] border-whites-1100 rounded-md'
               >
                 <div className='flex justify-between items-center'>
                   <Typography
                     weight='medium'
                     font='poppins'
-                    className='text-whites-900 mx-1 text-[12px] md:text-[14px]'
+                    className='text-whites-100 px-5 mx-1 rounded-md bg-primary-600 lg:text-[12px]'
+                  >
+                    Lanyard
+                  </Typography>
+                  <Typography
+                    weight='medium'
+                    font='poppins'
+                    className='text-whites-100 px-5 mx-1 rounded-md bg-primary-600 lg:text-[12px]'
+                  >
+                    Lanyard
+                  </Typography>
+                  <Typography
+                    weight='medium'
+                    font='poppins'
+                    className='text-whites-100 px-5 mx-1 rounded-md bg-primary-600 lg:text-[12px]'
+                  >
+                    Lanyard
+                  </Typography>
+                  <IoIosArrowDown className='flex' />
+                </div>
+                {showCatDropdown && (
+                  <div className='flex-col -ml-2 bg-whites-100 py-4 mt-2 lg:w-80 w-28 h-16 flex justify-center p-2 border-[1px] shadow-40 rounded-md'>
+                    <Typography
+                      weight='medium'
+                      font='poppins'
+                      className='text-whites-900 text-[12px] lg:text-[14px] mx-1 my-1 hover:bg-whites-900 hover:text-whites-100 hover:rounded-sm hover:px-1'
+                    >
+                      Lanyard
+                    </Typography>
+                    <Typography
+                      weight='medium'
+                      font='poppins'
+                      className='text-whites-900 text-[12px] lg:text-[14px] mx-1 my-1 hover:bg-whites-900 hover:text-whites-100 hover:rounded-sm hover:px-1'
+                    >
+                      Topi
+                    </Typography>
+                  </div>
+                )}
+              </div>
+              <div
+                onClick={SortDropdownClick}
+                style={{ cursor: 'pointer' }}
+                className='hidden cursor-pointer z-10 lg:block items-center flex-col bg-whites-100 lg:w-64 w-28 h-10 p-2 border-[1px] border-whites-1100 rounded-md'
+              >
+                <div className='flex justify-between items-center'>
+                  <Typography
+                    weight='medium'
+                    font='poppins'
+                    className='text-whites-900 mx-1 text-[12px] lg:text-[14px]'
                   >
                     Sort
                   </Typography>
                   <IoIosArrowDown className='flex' />
                 </div>
-                {showDropdown && (
-                  <div className='flex-col -ml-2 box-content py-4 mt-2 md:w-48 sm:w-36 w-28 h-8 flex justify-center p-2 border-[1px] shadow-40 rounded-md'>
+                {showSortDropdown && (
+                  <div className='flex-col -ml-2 bg-whites-100 py-4 mt-2 lg:w-64 w-28 h-16 flex justify-center p-2 border-[1px] shadow-40 rounded-md'>
                     <Typography
                       weight='medium'
                       font='poppins'
-                      className='text-whites-900 text-[12px] md:text-[14px] mx-1 my-1 hover:bg-whites-900 hover:text-whites-100 hover:rounded-sm hover:px-1'
+                      className='text-whites-900 text-[12px] lg:text-[14px] mx-1 my-1 hover:bg-whites-900 hover:text-whites-100 hover:rounded-sm hover:px-1'
                     >
                       Termurah
                     </Typography>
                     <Typography
                       weight='medium'
                       font='poppins'
-                      className='text-whites-900 text-[12px] md:text-[14px] mx-1 my-1 hover:bg-whites-900 hover:text-whites-100 hover:rounded-sm hover:px-1'
+                      className='text-whites-900 text-[12px] lg:text-[14px] mx-1 my-1 hover:bg-whites-900 hover:text-whites-100 hover:rounded-sm hover:px-1'
                     >
                       Termahal
                     </Typography>
@@ -153,31 +244,61 @@ export default function PageMerch() {
               </div>
             </div>
           </div>
-          <div className='mt-12 sm:mx-8 mx-4 flex flex-wrap justify-around'>
+          <div className='mt-4 mx-4 sm:mx-6 flex flex-wrap justify-around'>
             {products.map((products) => (
-              <div key={products.id} className='m-4 my-6'>
-                <div className='bg-primary-300 shadow-80 w-fit px-3 pt-3 pb-8 sm:px-5 sm:pt-5 sm:pb-10 rounded-xl'>
-                  <NextImage
-                    src={products.imageSrc}
-                    alt='productimage'
-                    width={75}
-                    height={75}
-                    className='w-[75px] sm:w-[90px] md:w-[105px] lg:w-[120px] xl:w-[135px]'
-                  />
-                  <Typography
-                    weight='medium'
-                    font='poppins'
-                    className='text-whites-1100 text-[12px] md:text-[14px] lg:text-[16px] xl:text-[18px]'
-                  >
-                    {products.name}
-                  </Typography>
-                  <Typography
-                    weight='bold'
-                    font='poppins'
-                    className='text-whites-1100 text-[12px] md:text-[14px] lg:text-[16px] xl:text-[18px]'
-                  >
-                    {products.price}
-                  </Typography>
+              <div key={products.id} className='my-3 mx-1 sm:mx-2'>
+                <div className=''>
+                  <div className=''>
+                    <NextImage
+                      src={products.imageSrc}
+                      alt='productimage'
+                      width={100}
+                      height={100}
+                      className='w-[120px] sm:w-[150px] md:w-[170px] lg:w-[325px]'
+                    />
+                  </div>
+                  <div className=''>
+                    <Typography
+                      weight='medium'
+                      font='poppins'
+                      className='text-whites-100 absolute -mt-72 ml-[220px] px-5 py-1 rounded-md bg-primary-600 hidden lg:block lg:text-[13px]'
+                    >
+                      {products.category}
+                    </Typography>
+                  </div>
+                  <div className='bg-primary-600 shadow-80 rounded-b-lg p-3 -mt-3 lg:-mt-8 lg:p-5'>
+                    <div className='lg:flex lg:justify-between lg:items-center'>
+                      <div className='lg:flex-col'>
+                        <Typography
+                          weight='medium'
+                          font='poppins'
+                          className='text-whites-100 text-[12px] sm:text-[14px] md:text-[16px] lg:text-[16px] xl:text-[18px]'
+                        >
+                          {products.name}
+                        </Typography>
+                        <Typography
+                          weight='bold'
+                          font='poppins'
+                          className='text-whites-100 text-[12px] sm:text-[14px] md:text-[16px] lg:text-[16px] xl:text-[18px]'
+                        >
+                          {products.price}
+                        </Typography>
+                      </div>
+                      <button className='border-[1px] border-whites-100 p-2 lg:w-20 w-full h-fit rounded-md mt-2 flex relative justify-center items-center'>
+                        <Typography
+                          weight='bold'
+                          font='poppins'
+                          className='text-whites-100 text-[12px] sm:text-[14px] md:text-[16px] lg:text-[16px] xl:text-[18px]'
+                        >
+                          Beli
+                        </Typography>
+                        <PiShoppingCartSimpleFill
+                          color='#ffffff'
+                          className='mx-1 w-4'
+                        />
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             ))}
