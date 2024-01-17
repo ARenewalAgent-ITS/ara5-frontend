81982a055c5f7bdc2d96fccd8e56749d12599619
unknown <wisnuyeahfaizal@gmail.com>
2024-01-17 18:44:01 +0700

    fix: set merch to coming soon

--- src/app/merch/page.tsx
+++ src/app/merch/page.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
-import SEO from '@/components/SEO';
-import PageMerch from '@/containers/merchPage/PageMerch';
+// import SEO from '@/components/SEO';
+// import PageMerch from '@/containers/merchPage/PageMerch';
+import ComingSoon from '@/app/comingsoon/page';
 
 export default function page() {
   return (
     <>
-      <SEO
+      {/* <SEO
         title='Merchandise'
         description='ARA (A Renewal Agent) 5.0 adalah kegiatan yang diselenggarakan oleh HMIT (Himpunan Mahasiswa Teknologi Informasi) ITS periode 2022-2023 yang dimana event ini akan menjadi media untuk menyalurkan minat di bidang IT (teknologi informasi) bagi siswa SMA/SMK dan mahasiswa.'
-      />
-      <PageMerch />
+      /> */}
+      {/* <PageMerch /> */}
+      <ComingSoon />
     </>
   );
 }
