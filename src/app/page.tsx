f1f16404a003523d00fc387a529f72b7bb302a25
fioreenza <fiorenzanalle05@gmail.com>
2024-05-01 01:06:11 +0700

    add image and fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Footer from '@/components/layouts/Footer';
 import Navbar from '@/components/layouts/Navbar';
 import SEO from '@/components/SEO';
 import LandingPage from '@/containers/landingPage/LandingPage';
+import VotePage from '@/containers/exploitPage/VotePage';
 
 export default function Page() {
   return (
@@ -12,9 +13,7 @@ export default function Page() {
         title='Home'
         description='ARA ITS (A Renewal Agent ITS) 5.0 adalah kegiatan yang diselenggarakan oleh HMIT (Himpunan Mahasiswa Teknologi Informasi) ITS periode 2023-2024 yang dimana event ini akan menjadi media untuk menyalurkan minat di bidang IT (teknologi informasi) bagi siswa SMA/SMK dan mahasiswa.'
       />
-      <Navbar />
-      <LandingPage />
-      <Footer />
+      <VotePage />
     </>
   );
 }
