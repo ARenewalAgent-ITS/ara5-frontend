b24d1b33cc4c92c99dc4f025532288dde5b5bbd8
Rifqi Akhmad Maulana <rifki.ahmad2003@gmail.com>
2024-05-08 14:04:18 +0700

    feat: vote functionality fix and implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,17 @@ import Footer from '@/components/layouts/Footer';
 import Navbar from '@/components/layouts/Navbar';
 import SEO from '@/components/SEO';
 import LandingPage from '@/containers/landingPage/LandingPage';
-import VotePage from '@/containers/exploitPage/VotePage';
 
 export default function Page() {
   return (
     <>
-      <VotePage />
+      <SEO
+        title='Home'
+        description='ARA ITS (A Renewal Agent ITS) 5.0 adalah kegiatan yang diselenggarakan oleh HMIT (Himpunan Mahasiswa Teknologi Informasi) ITS periode 2023-2024 yang dimana event ini akan menjadi media untuk menyalurkan minat di bidang IT (teknologi informasi) bagi siswa SMA/SMK dan mahasiswa.'
+      />
+      <Navbar />
+      <LandingPage />
+      <Footer />
     </>
   );
 }
